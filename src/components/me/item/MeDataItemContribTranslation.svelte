f2668102783794dc9e9b1a19afc5d4245d836424
somni <me@somni.one>
2025-02-25 03:22:08 +0900

    Add proof link for translation contributed on Weblate

--- src/components/me/item/MeDataItemContribTranslation.svelte
+++ src/components/me/item/MeDataItemContribTranslation.svelte
@@ -1,19 +1,32 @@
 <MeDataItemBase idPrefix="contribution-translation"
                 item={item}>
   <ul>
-    {#each item.githubPullRequests ?? [] as pullRequest}
-      <li>
-        <MeDataGitHubPullRequestItem repositoryUrl={item.repositoryUrl}
-                                     pullRequestNumber={pullRequest} />
-      </li>
-    {/each}
+    {#if item.platform === "github"}
+      {#each item.githubPullRequests ?? [] as pullRequest}
+        <li>
+          <MeDataGitHubPullRequestItem repositoryUrl={item.repositoryUrl}
+                                      pullRequestNumber={pullRequest} />
+        </li>
+      {/each}
 
-    {#each item.directCommits ?? [] as directCommit}
-      <li>
-        <MeDataGitHubCommitItem repositoryUrl={item.repositoryUrl}
-                                commitHash={directCommit} />
-      </li>
-    {/each}
+      {#each item.directCommits ?? [] as directCommit}
+        <li>
+          <MeDataGitHubCommitItem repositoryUrl={item.repositoryUrl}
+                                  commitHash={directCommit} />
+        </li>
+      {/each}
+    {/if}
+
+    <li>
+      {#if item.platform === "weblate"}
+        <a href={item.proofUrl}
+           class="inline-block *:mr-2">
+          <span class="float-left w-6 h-6"
+                style:fill="#{siWeblate.hex}">{@html siWeblate.svg}</span>
+          <span>{$_("me.contrib.translation.weblate.proof")}</span>
+        </a>
+      {/if}
+    </li>
   </ul>
 </MeDataItemBase>
 
@@ -22,6 +35,8 @@ import MeDataItemBase from "$/components/me/item/base/MeDataItemBase.svelte";
 import MeDataGitHubCommitItem from "$/components/me/MeDataGitHubCommitItem.svelte";
 import MeDataGitHubPullRequestItem from "$/components/me/MeDataGitHubPullRequestItem.svelte";
 import type { MeContributionTranslationData } from "$/lib/typings/me-data";
+import { _ } from "svelte-i18n";
+import { siWeblate } from "simple-icons";
 
 const { item }: { item: MeContributionTranslationData } = $props();
 </script>
