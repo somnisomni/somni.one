009260f52b1b4f4b6840ac336ca3272cb3906ed9
somni <me@somni.one>
2025-02-25 03:21:24 +0900

    Add OpenWrt translation contrib data

--- src/lib/typings/me-data.ts
+++ src/lib/typings/me-data.ts
@@ -19,6 +19,7 @@ export interface MeProjectDataBase extends MeDataBase {
 
 export interface MeContributionDataBase extends MeDataBase {
   repositoryUrl: string;
+  proofUrl?: string;
 }
 
 export type MeProjectIndividualData = MeProjectDataBase;
