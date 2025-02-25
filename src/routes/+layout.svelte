37e202eebee2f3d661918ed66a6d15bfe8293c90
somni <me@somni.one>
2025-02-25 22:19:47 +0900

    Add route loading progress bar

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,9 +1,12 @@
+<ProgressBar class="text-sky-300" />
+
 <main>
   {@render children()}
 </main>
 
 <script lang="ts">
 import "$/styles/app.css";
+import { ProgressBar } from "@prgm/sveltekit-progress-bar";
 import type { LayoutProps } from "./$types";
 
 const { children }: LayoutProps = $props();
