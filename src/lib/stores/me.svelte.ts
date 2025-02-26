7f0fdc39a622907e71386ac013a48232c6b0b633
somni <me@somni.one>
2025-02-26 23:45:50 +0900

    Fix `/me` store typing imports

--- src/lib/stores/me.svelte.ts
+++ src/lib/stores/me.svelte.ts
@@ -1,5 +1,4 @@
-import type { GitHubCommitData } from "$/routes/me/api/github/commit/+server";
-import type { GitHubPullRequestData } from "$/routes/me/api/github/pull-request/+server";
+import type { GitHubPullRequestData, GitHubCommitData } from "$/lib/typings/github";
 
 export const githubPullRequestDataStore = $state<Record<string, GitHubPullRequestData>>({ });
 export const githubCommitDataStore = $state<Record<string, GitHubCommitData>>({ });
