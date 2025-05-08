3f46f50b6fdbac7e847622ebfdf438e9bbd0fe70
somni <me@somni.one>
2025-05-09 04:39:42 +0900

    Fix redirection of `/me`

--- src/routes/me/+page.server.ts
+++ src/routes/me/+page.server.ts
@@ -1,9 +1,8 @@
-import type { LayoutServerLoad } from "./$types";
 import { redirect } from "@sveltejs/kit";
 
-export const load: LayoutServerLoad = async ({ route }) => {
+export function load({ route }): void {
   /* Redirect to default subpage (individual projects) if the user navigated to `/me` */
   if(route.id?.endsWith("/me")) {
     redirect(308, "/me/individual");
   }
-};
+}
