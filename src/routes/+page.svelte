8d3334676802822ff9cf74a997f58b1abad03b99
somni <me@somni.one>
2025-05-09 04:44:00 +0900

    Rewrite main page

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,54 +1,130 @@
-<div class="main-page">
-  {#each data as item}
-    <DomainItem data={ item } />
-  {/each}
-</div>
+<svelte:window on:keydown={ onKeyDown } />
+
+<section id="headline">
+  <div class="max-w-full">
+    <h1 class="headline-text">
+      <p>somni</p>
+      <p>Turns Out,</p>
+      <p>I've Been</p>
+      <p>Keep <strong>Trying</strong>.</p>
+      <p class="user-typings">
+        <span>
+          <a bind:this={ additionalTypingsUrlAnchor }
+             class="user-typings-url"
+             href={ additionalTypingsUrl }>
+            {@html additionalTypingsHtml}
+          </a>
+        </span><!--
+
+     --><span class="cursor ml-2">▏</span>
+        {#if additionalTypingsUrl }
+          <span in:fade out:fade
+                class="user-typings-url-hint">Ctrl + Enter</span>
+        {/if}
+      </p>
+    </h1>
+  </div>
+</section>
 
 <script lang="ts">
-import type { DomainItemData } from "$lib/types";
-import DomainItem from "$components/DomainItem.svelte";
+import { fade } from "svelte/transition";
 
-const data: DomainItemData[] = [
-  {
-    title: [
-      { text: "somni", description: "my nickname" },
-      { text: "." },
-      { text: "one", description: "i'm the one" },
-    ],
-    href: "https://somni.one",
-    subdomains: [
-      {
-        title: "> whoami",
-        href: "https://somni.one/me",
-      },
-      {
-        title: "> blog",
-        href: "https://log.somni.one",
-      },
-    ],
-  },
-  {
-    title: [
-      { text: "koharu", description: "one of my favorite character", forceColor: "#DEA0AC" },
-      { text: "." },
-      { text: "dev", description: "my dev space" },
-    ],
-    href: "https://koharu.dev",
-  },
-  {
-    title: [
-      { text: "miku", description: "you know who she is", forceColor: "#39C5BB" },
-      { text: "." },
-      { text: "party", description: "just one of available TLDs" },
-    ],
-    href: "https://miku.party",
-  },
-];
+const typingUrlMap: Record<string, string> = {
+  "mail": "mailto:" + atob("bWU=") + "@" + atob("c29tbmkub25l"),
+  "email": "mailto:" + atob("bWU=") + "@" + atob("c29tbmkub25l"),
+
+  "twitter": "https://twitter.com/somni_somni",
+  "x": "https://twitter.com/somni_somni",
+
+  "github": "https://github.com/somnisomni",
+  "git": "https://github.com/somnisomni",
+
+  "blog": "https://log.somni.one",
+  "log": "https://log.somni.one",
+
+  "me": "/me",
+  "portfolio": "/me",
+  "project": "/me",
+  "projects": "/me",
+  "work": "/me",
+  "works": "/me",
+  "resume": "/me",
+  "cv": "/me",
+};
+
+let additionalTypings: string = $state("");
+let additionalTypingsHtml: string = $derived(additionalTypings.replace(/ /g, "&nbsp;"));
+let additionalTypingsUrl: string = $derived(
+  additionalTypings.toLowerCase() in typingUrlMap
+    ? typingUrlMap[additionalTypings.toLowerCase()]
+    : "");
+let additionalTypingsUrlAnchor: HTMLAnchorElement | null = null;
+
+function onKeyDown(event: KeyboardEvent) {
+  if(event.key.match(/^[a-zA-Z0-9`!@#$%^&*,.;:'"<>(){}\[\]/\\ ]$/)
+    && additionalTypings.length <= 20) {
+    additionalTypings += event.key;
+  } else if(event.key === "Backspace") {
+    additionalTypings = additionalTypings.slice(0, -1);
+  } else if(event.key === "Enter" && event.ctrlKey && additionalTypingsUrlAnchor) {
+    additionalTypingsUrlAnchor.click();
+  } else if(event.key === "Escape") {
+    additionalTypings = "";
+  }
+}
 </script>
 
-<style lang="scss">
-.main-page {
-  width: 100%;
-  min-height: 100vh;
+<style scoped>
+@reference "$/styles/app.css";
+
+section {
+  @apply flex w-full h-screen items-center justify-center;
+}
+
+section#headline {
+  @apply flex-col items-start p-8;
+}
+
+section#headline .headline-text {
+  @apply flex flex-col font-medium;
+
+  font-size: 8rem;
+  line-height: 1.2;
+}
+
+section#headline .headline-text > :first-child,
+section#headline .headline-text > :last-child {
+  @apply text-zinc-400;
+
+  font-size: 0.5em;
+  line-height: 3;
+}
+
+section#headline .headline-text strong {
+  @apply font-black;
+}
+
+section#headline .user-typings {
+  @apply break-all font-mono;
+}
+
+section#headline a.user-typings-url[href=""] {
+  @apply pointer-events-none;
+}
+
+section#headline a.user-typings-url:not([href=""]) {
+  @apply underline;
+}
+
+section#headline .user-typings-url-hint {
+  @apply font-light opacity-50;
+
+  font-size: 0.5em;
+}
+
+section#headline .cursor {
+  @apply pointer-events-none select-none;
+
+  animation: fadeInOut 1.5s cubic-bezier(0.5, 0, 0.5, 1) infinite;
 }
 </style>
