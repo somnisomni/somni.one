5b2830528bcac03956e8d0a7157af8ae6d82b047
somni <me@somni.one>
2025-05-10 02:47:02 +0900

    Add GSAP package

---
+++ src/lib/init.ts
@@ -0,0 +1,6 @@
+import { gsap } from "gsap";
+import { SplitText } from "gsap/SplitText";
+import { TextPlugin } from "gsap/TextPlugin";
+
+// GSAP initialization
+gsap.registerPlugin(SplitText, TextPlugin);
