cd6d359173ccef8e2468db5bf56844d76075464d
somni <me@somni.one>
2025-08-03 23:36:29 +0900

    Remove data collection related scripts from frontend

--- projects/web/src/lib/server/octokit.ts
+++
@@ -1,7 +0,0 @@
-import { Octokit } from "octokit";
-
-export function createOctokit(useAuth = false): Octokit {
-  return new Octokit({
-    auth: (useAuth && typeof import.meta.env.VITE_GITHUB_PAT === "string") ? import.meta.env.VITE_GITHUB_PAT : undefined,
-  });
-}
--- projects/web/src/routes/+page.server.ts
+++ projects/web/src/routes/+page.server.ts
@@ -3,7 +3,7 @@ import type { PageServerLoad } from "./$types";
 
 export const load: PageServerLoad = async () => {
   // Load data from JSON
-  const linkData = [ ...(await import("$/lib/server/data/links.json")).default as LinkCategory[] ];
+  const linkData = [ ...(await import("$/lib/data/links.json")).default as LinkCategory[] ];
 
   // Return data
   return { linkData };
