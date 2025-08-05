3508df83b4abd416f62fabdb605ffe040dfd3dd5
somni <me@somni.one>
2025-08-06 01:07:26 +0900

    Use lowercase status string in status chip

--- projects/web/src/components/works/fragments/WorkStatusChip.svelte
+++ projects/web/src/components/works/fragments/WorkStatusChip.svelte
@@ -1,5 +1,5 @@
-<div class="item-data-status-chip text-{statusBgColorMap[status]}-900 bg-{statusBgColorMap[status]}-300">
-  {$_(`me.common.status.${status.toLowerCase()}`)}
+<div class="item-data-status-chip text-{statusBgColorMap[statusLowerCase]}-900 bg-{statusBgColorMap[statusLowerCase]}-300">
+  {$_(`me.common.status.${statusLowerCase}`)}
 </div>
 
 <script lang="ts">
@@ -7,8 +7,9 @@ import type { ProjectDataBase } from "@somni.one/common";
 import { _ } from "svelte-i18n";
 
 const { status }: { status: ProjectDataBase["status"] } = $props();
+const statusLowerCase = $derived(status.toLowerCase() as Lowercase<ProjectDataBase["status"]>);
 
-const statusBgColorMap: Record<typeof status, string> = {
+const statusBgColorMap: Record<typeof statusLowerCase, string> = {
   "completed": "green",    // text-green-900  bg-green-300
   "discontinued": "red",   // text-red-900    bg-red-300
   "maintaining": "yellow", // text-yellow-900 bg-yellow-300
