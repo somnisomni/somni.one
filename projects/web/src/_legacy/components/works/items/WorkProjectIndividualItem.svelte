19734df718c5fcee3e81b8585fb599405aee6afa
somni <me@somni.one>
2025-12-04 20:19:21 +0900

    Delete legacy files

--- projects/web/src/_legacy/components/works/items/WorkProjectIndividualItem.svelte
+++
@@ -1,9 +0,0 @@
-<WorkProjectItemBase idTargetPrefix="individual"
-                     item={item} />
-
-<script lang="ts">
-import WorkProjectItemBase from "$/components/works/items/base/WorkProjectItemBase.svelte";
-import type { ProjectIndividualData } from "@somni.one/common";
-
-const { item }: { item: ProjectIndividualData } = $props();
-</script>
--- projects/web/src/_legacy/routes/works/+page.server.ts
+++
@@ -1,8 +0,0 @@
-import { redirect } from "@sveltejs/kit";
-
-export function load({ route }): void {
-  /* Redirect to default subpage (individual projects) if the user navigated to `/works` */
-  if(route.id?.endsWith("/works")) {
-    redirect(308, "/works/individual");
-  }
-}
