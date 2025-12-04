129ac6d29298a98d09f8f1f520fd66b4ae6a58ea
somni <me@somni.one>
2025-12-04 23:41:13 +0900

    Create schema directory if not exists

--- projects/common/tools/generate-json-schemas.mjs
+++ projects/common/tools/generate-json-schemas.mjs
@@ -1,26 +1,33 @@
 import tsj from "ts-json-schema-generator";
 import fs from "fs";
 
-/** @type {(import("ts-json-schema-generator").Config & { output: string })[]} */
+/** @type {(import("ts-json-schema-generator").Config & { outputFileName: string })[]} */
 const configs = [
   {
     path: "src/typings/works/index.ts",
     type: "ProjectDataSchema",
-    output: "schema/works_projects.json",
+    outputFileName: "works_projects.json",
   },
   {
     path: "src/typings/works/index.ts",
     type: "ContributionTranslationDataSchema",
-    output: "schema/works_contributions_translation.json",
+    outputFileName: "works_contributions_translation.json",
   },
   {
     path: "src/typings/works/index.ts",
     type: "ContributionOpenSourceDataSchema",
-    output: "schema/works_contributions_opensource.json",
+    outputFileName: "works_contributions_opensource.json",
   },
 ];
+const schemaDir = "schema/";
 const tsconfigPath = "tsconfig.json";
 
+// Create schema directory if it doesn't exist
+if(!fs.existsSync(schemaDir)) {
+  fs.mkdirSync(schemaDir);
+}
+
+// Generate and write schemas
 for(const config of configs) {
   const chema = tsj.createGenerator({
     ...config,
@@ -30,7 +37,7 @@ for(const config of configs) {
   }).createSchema(config.type);
   const schemaString = JSON.stringify(chema, null, 2);
 
-  fs.writeFile(config.output, schemaString, err => {
+  fs.writeFile(`${schemaDir}${config.outputFileName}`, schemaString, err => {
     if(err) throw err;
   });
 }
