7922b8eb513828ab0cfb2b7f372b62e6072b86f6
somni <me@somni.one>
2025-12-19 10:35:32 +0900

    Update game projects page

--- projects/web/src/routes/works/games/+page.svelte
+++ projects/web/src/routes/works/games/+page.svelte
@@ -1,7 +1,7 @@
 <div class="page">
   <div class="nav-desc">
     <p>&nbsp;이전에는 단순히 게임을 플레이하는 것을 좋아했지만, 매우 다양한 창작 분야가 모여 하나의 작품을 이루는 <i>"창작의 정수"</i>라는 점을 알게 되었고 이에 매료되어 게임 개발자의 길을 걷게 되었습니다.</p>
-    <p>&nbsp;현재 <b><IconText svg={ siUnity.svg }>Unity</IconText></b>를 주력으로 다루고 있고, <b><IconText svg={ siUnrealengine.svg }>Unreal Engine</IconText></b>을 기본적인 수준으로 다룰 수 있습니다.</p>
+    <p>&nbsp;현재 <b><IconText svg={ siUnity.svg }>Unity</IconText></b>를 주력으로 다루고 있습니다.</p>
     <p>&nbsp;게임의 <b>적응형 사운드</b>에 관심이 많으며 주력 사운드 미들웨어는 <IconText svg={ siFmod.svg }><b>FMOD</b></IconText>입니다. 이를 이용해 사운드 이벤트를 구성하고 인게임에 적용한 경험이 있습니다.</p>
   </div>
 
@@ -24,7 +24,7 @@
 </div>
 
 <script lang="ts">
-import { siUnity, siUnrealengine, siFmod } from "simple-icons";
+import { siUnity, siFmod } from "simple-icons";
 import IconText from "$/components/IconText.svelte";
 import GameWorks from "@somni.one/common/data/works/projects/game.json";
 import type { ProjectData } from "@somni.one/common";
